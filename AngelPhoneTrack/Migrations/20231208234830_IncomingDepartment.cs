57cc0693181d283606e8671907ca07c9cc4c71af
Sarmad Wahab <me@bin.moe>
2023-12-08 17:49:47 -0600

    Add migrations

---
+++ AngelPhoneTrack/Migrations/20231208234830_IncomingDepartment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngelPhoneTrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncomingDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IncomingDepartmentId",
+                table: "Assignments",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncomingDepartmentId",
+                table: "Assignments");
+        }
+    }
+}
